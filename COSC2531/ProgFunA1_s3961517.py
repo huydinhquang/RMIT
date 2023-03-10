0917e9aaa3670ddf43ca12f7bff324feb29f8432
Huy Quang Dinh <qhi1903@gmail.com>
2023-03-10 15:16:44 +1100

    Add menu for Part 2

--- COSC2531/ProgFunA1_s3961517.py
+++ COSC2531/ProgFunA1_s3961517.py
@@ -6,6 +6,7 @@
 
 # Define constants
 CONS_DASH = '-'
+CONS_SHARP = '*'
 CONS_TAB = '\t'
 CONS_BREAK_LINE = '\n'
 CONS_COMMA_WITH_SPACE = ', '
@@ -21,7 +22,33 @@ CONS_TICKET_QUANTITY_MIN = 0
 CONS_TICKET_QUANTITY_MAX = 50
 
 # Menu
-# Prepare for menu
+# Prepare data for menu
+sharp_mgs = CONS_SHARP * 50
+
+def break_line_message(message):
+    return ('{0}{1}').format(message, CONS_BREAK_LINE)
+
+# Prepare content for menu
+menu_mgs = ''
+menu_mgs += ('Welcome to the RMIT movie ticketing system!{0}').format(CONS_BREAK_LINE * 2)
+menu_mgs_list = [
+    sharp_mgs,
+    'You can choose from the following options:',
+    '1: Purchase a ticket',
+    '2: Add new movies',
+    '3: Display existing customers information',
+    '4: Display existing movies information',
+    '0: Exit the program',
+    sharp_mgs
+]
+for i in menu_mgs_list:
+    menu_mgs += break_line_message(i)
+
+# Print out the menu
+print(menu_mgs)
+
+option_selected = input('Choose one option: ')
+print(option_selected)
 
 # Function to convert the input value of reward program to Boolean
 def convert_answer_reward_program(is_reward_program):
@@ -109,67 +136,69 @@ def define_reward_program(is_reward_program_list):
         except ValueError:
             continue
 
-# # Ask for customer name
-#customer_name = input('Enter the name of the customer [e.g. Huy]:{0}'.format(CONS_BREAK_LINE))
-customer_name = 'James'
+# Function to purchase a ticket (Option 1)
+def purchase_ticket():
+    # # Ask for customer name
+    #customer_name = input('Enter the name of the customer [e.g. Huy]:{0}'.format(CONS_BREAK_LINE))
+    customer_name = 'James'
 
-# Ask for movie name
-movie_list = ['Avatar', 'Titanic', 'StarWar']
-movie_name = define_movie_name(movie_list)
+    # Ask for movie name
+    movie_list = ['Avatar', 'Titanic', 'StarWar']
+    movie_name = define_movie_name(movie_list)
 
-# Ask for ticket type
-ticket_type_list = ['adult', 'child', 'senior', 'concession']
-ticket_type = define_ticket_type(ticket_type_list)
+    # Ask for ticket type
+    ticket_type_list = ['adult', 'child', 'senior', 'concession']
+    ticket_type = define_ticket_type(ticket_type_list)
 
-# Ask for ticket quantity
-ticket_quantity = define_ticket_quantity()
+    # Ask for ticket quantity
+    ticket_quantity = define_ticket_quantity()
 
-customer_list = [
-    {CONS_CUSTOMER_NAME: 'Mary', CONS_REWARD_PROGRAM: True},
-    {CONS_CUSTOMER_NAME: 'James', CONS_REWARD_PROGRAM: False},
-]
+    customer_list = [
+        {CONS_CUSTOMER_NAME: 'Mary', CONS_REWARD_PROGRAM: True},
+        {CONS_CUSTOMER_NAME: 'James', CONS_REWARD_PROGRAM: False},
+    ]
 
-# Ask for joining reward program
-is_reward_program_list = ['y', 'n']
-is_reward_program = check_reward_program(customer_list, customer_name)
-if not is_reward_program:
-    is_reward_program = define_reward_program(is_reward_program_list)
+    # Ask for joining reward program
+    is_reward_program_list = ['y', 'n']
+    is_reward_program = check_reward_program(customer_list, customer_name)
+    if not is_reward_program:
+        is_reward_program = define_reward_program(is_reward_program_list)
 
-# Define list of unit price based on ticket type
-unit_price_list = {'adult': 25.0, 'child': 19.5, 'senior': 17.0, 'student': 20.5, 'concession': 20.5}
+    # Define list of unit price based on ticket type
+    unit_price_list = {'adult': 25.0, 'child': 19.5, 'senior': 17.0, 'student': 20.5, 'concession': 20.5}
 
-# Calculate ticket and booking fees
-unit_price = unit_price_list[ticket_type]
-booking_fee = ticket_quantity * 2
-ticket_cost = ticket_quantity * unit_price
+    # Calculate ticket and booking fees
+    unit_price = unit_price_list[ticket_type]
+    booking_fee = ticket_quantity * 2
+    ticket_cost = ticket_quantity * unit_price
 
-# Calculate discount based on the reward program
-discount = 0
-if is_reward_program:
-    discount = ticket_cost * CONS_REWARD_PROGRAM_PERCENT / 100
+    # Calculate discount based on the reward program
+    discount = 0
+    if is_reward_program:
+        discount = ticket_cost * CONS_REWARD_PROGRAM_PERCENT / 100
 
-# Calculate total price
-total_cost = ticket_cost - discount + booking_fee
+    # Calculate total price
+    total_cost = ticket_cost - discount + booking_fee
 
-# Prepare data
-dash_mgs = CONS_DASH*50 + CONS_BREAK_LINE
-tab_mgs_twice = CONS_TAB*2
-tab_mgs_three = CONS_TAB*3
-tab_mgs_four = CONS_TAB*4
+    # Prepare data for receipt
+    dash_mgs = CONS_DASH * 50 + CONS_BREAK_LINE
+    tab_mgs_twice = CONS_TAB * 2
+    tab_mgs_three = CONS_TAB * 3
+    tab_mgs_four = CONS_TAB * 4
 
-# Prepare content for receipt
-receipt_message = ''
-receipt_message += dash_mgs
-receipt_message += ('Receipt of {0}{1}').format(customer_name, CONS_BREAK_LINE)
-receipt_message += dash_mgs
-receipt_message += ('Movie:{0}{1}{2}').format(tab_mgs_four, movie_name, CONS_BREAK_LINE)
-receipt_message += ('Ticket type:{0}{1}{2}').format(tab_mgs_three, ticket_type, CONS_BREAK_LINE)
-receipt_message += ('Ticket unit price:{0}{1}{2}').format(tab_mgs_twice, unit_price, CONS_BREAK_LINE)
-receipt_message += ('Ticket quantity:{0}{1}{2}').format(tab_mgs_twice, ticket_quantity, CONS_BREAK_LINE)
-receipt_message += dash_mgs
-receipt_message += ('Discount:{0}{1}{2}').format(tab_mgs_three, discount, CONS_BREAK_LINE)
-receipt_message += ('Booking fee:{0}{1}{2}').format(tab_mgs_three, booking_fee, CONS_BREAK_LINE)
-receipt_message += ('Total cost:{0}{1}{2}').format(tab_mgs_three, total_cost, CONS_BREAK_LINE)
+    # Prepare content for receipt
+    receipt_mgs = ''
+    receipt_mgs += dash_mgs
+    receipt_mgs += ('Receipt of {0}{1}').format(customer_name, CONS_BREAK_LINE)
+    receipt_mgs += dash_mgs
+    receipt_mgs += ('Movie:{0}{1}{2}').format(tab_mgs_four, movie_name, CONS_BREAK_LINE)
+    receipt_mgs += ('Ticket type:{0}{1}{2}').format(tab_mgs_three, ticket_type, CONS_BREAK_LINE)
+    receipt_mgs += ('Ticket unit price:{0}{1}{2}').format(tab_mgs_twice, unit_price, CONS_BREAK_LINE)
+    receipt_mgs += ('Ticket quantity:{0}{1}{2}').format(tab_mgs_twice, ticket_quantity, CONS_BREAK_LINE)
+    receipt_mgs += dash_mgs
+    receipt_mgs += ('Discount:{0}{1}{2}').format(tab_mgs_three, discount, CONS_BREAK_LINE)
+    receipt_mgs += ('Booking fee:{0}{1}{2}').format(tab_mgs_three, booking_fee, CONS_BREAK_LINE)
+    receipt_mgs += ('Total cost:{0}{1}{2}').format(tab_mgs_three, total_cost, CONS_BREAK_LINE)
 
-# Print out the receipt
-print(receipt_message)
+    # Print out the receipt
+    print(receipt_mgs)
