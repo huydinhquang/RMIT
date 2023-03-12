1e578b9cc38c2edbc8c47d36e680632fa87f62a9
huydinhquang <qhi1903@gmail.com>
2023-03-11 21:55:25 -0800

    Update to complete the purchase of multiple ticket types and ticket quantities for Part 3.1

--- COSC2531/ProgFunA1_s3961517.py
+++ COSC2531/ProgFunA1_s3961517.py
@@ -17,6 +17,9 @@ CONS_CUSTOMER_NAME = 'customer_name'
 CONS_CUSTOMER_REWARD_PROGRAM = 'reward_program'
 CONS_MOVIE_NAME = 'movie_name'
 CONS_MOVIE_SEAT = 'movie_seat'
+CONS_TICKET_TYPE = 'ticket_type'
+CONS_TICKET_UNIT_PRICE = 'ticket_unit_price'
+CONS_TICKET_QUANTITY = 'ticket_quantity'
 
 # Define value
 CONS_IS_REWARD_PROGRAM_YES = 'y'
@@ -27,7 +30,7 @@ CONS_TICKET_QUANTITY_MAX = 50
 # Menu
 # Prepare data for menu
 sharp_mgs = CONS_SHARP * 50
-dash_mgs = CONS_DASH * 50 + CONS_BREAK_LINE
+dash_mgs = CONS_DASH * 50
 
 # Initially, define movies and customers
 init_movie_list = [
@@ -40,7 +43,9 @@ init_customer_list = [
         {CONS_CUSTOMER_NAME: 'Mary', CONS_CUSTOMER_REWARD_PROGRAM: True},
         {CONS_CUSTOMER_NAME: 'James', CONS_CUSTOMER_REWARD_PROGRAM: False},
     ]
-    
+
+init_ticket_type_list = ['adult', 'child', 'senior', 'concession']
+
 def break_line_message(message):
     return ('{0}{1}').format(message, CONS_BREAK_LINE)
 
@@ -57,6 +62,8 @@ menu_mgs_list = [
     '2: Add new movies',
     '3: Display existing customers information',
     '4: Display existing movies information',
+    '5: Display the most popular movie',
+    '6: Display all movie record',
     '0: Exit the program',
     sharp_mgs
 ]
@@ -99,6 +106,18 @@ def validate_available_ticket_quantity(movie_name, ticket_quantity):
         return True
     return False
 
+def validate_movie_ticket_quantity(ticket_quantity):
+    try:
+        ticket_quantity = int(ticket_quantity)
+        if ticket_quantity <= CONS_TICKET_QUANTITY_MIN:
+            print('The ticket quantity is not valid. Please enter a valid ticket quantity.')
+            return False
+
+        return True
+    except ValueError:
+        print('The ticket quantity is not valid. Please enter a valid ticket quantity.')
+        return False
+
 # Function to enter a valid movie
 def define_movie_name(init_movie_list):
     movie_list_str = CONS_COMMA_WITH_SPACE.join([m[CONS_MOVIE_NAME] for m in init_movie_list])
@@ -109,7 +128,8 @@ def define_movie_name(init_movie_list):
             # movie_name = 'Avatar'
             if not validate_predefined_value(init_movie_list, CONS_MOVIE_NAME, movie_name):
                 print('The movie is not valid. Please enter a valid movie!')
-            
+                continue
+
             return movie_name
         except ValueError:
             continue
@@ -119,12 +139,25 @@ def define_ticket_type(ticket_type_list):
     ticket_type_list_str = CONS_COMMA_WITH_SPACE.join(ticket_type_list)
     while True:
         try:
-            ticket_type = input(break_line_message('Enter the ticket type [enter a valid type only, e.g. {0}]:').format(ticket_type_list_str))
+            ticket_type_str = input(break_line_message('Enter the ticket type [enter a valid type only, e.g. {0}]:').format(ticket_type_list_str))
             # ticket_type = 'adult'
-            if not validate_predefined_value(value_list = ticket_type_list, value = ticket_type):
-                print('The ticket type is not valid. Please enter a valid ticket type.')
+            ticket_type_list = convert_string_to_list(ticket_type_str, CONS_COMMA)
             
-            return ticket_type
+            is_valid = True
+            result = []
+
+            for i in ticket_type_list:
+                i = i.strip() # Strip out all spaces before and after the item
+                if not validate_predefined_value(value_list = init_ticket_type_list, value = i):
+                    print(('The ticket type \'{0}\' is not valid. Please enter a valid ticket type.').format(i))
+                    is_valid = False
+                else:
+                    result.append(i)
+
+            if not is_valid:
+                continue
+
+            return result
         except ValueError:
             continue
 
@@ -132,20 +165,32 @@ def define_ticket_type(ticket_type_list):
 def define_ticket_quantity(movie_name):
     while True:
         try:
-            ticket_quantity = int(input(break_line_message('Enter the ticket quantity [enter a positive integer only, e.g. 1, 2, 3]')))
+            ticket_quantity_str = input(break_line_message('Enter the ticket quantity [enter a positive integer only, e.g. 1, 2, 3]'))
             # ticket_quantity = 20
+            ticket_quantity_list = convert_string_to_list(ticket_quantity_str, CONS_COMMA)
 
-            if ticket_quantity <= CONS_TICKET_QUANTITY_MIN:
-                print('The ticket quantity is not valid. Please enter a valid ticket quantity.')
-                continue
+            is_valid = True
+            total_ticket = 0
+            result = []
 
-            if validate_available_ticket_quantity(movie_name, ticket_quantity):
+            for i in ticket_quantity_list:
+                i = i.strip() # Strip out all spaces before and after the item
+                if not validate_movie_ticket_quantity(i):
+                    is_valid = False
+                else:
+                    ticket_quantity = int(i)
+                    total_ticket += ticket_quantity
+                    result.append(ticket_quantity)
+
+            if validate_available_ticket_quantity(movie_name, total_ticket):
                 print('Ticket quantity must be less than the number of available seats. Please enter a smaller ticket quantity.')
+                is_valid = False
+                        
+            if not is_valid:
                 continue
 
-            return ticket_quantity
+            return result, total_ticket
         except ValueError:
-            print('The ticket quantity is not valid. Please enter a valid ticket quantity.')
             continue
 
 # Function to enter a valid reward program
@@ -172,11 +217,10 @@ def purchase_ticket():
     movie_name = define_movie_name(init_movie_list)
 
     # Ask for ticket type
-    ticket_type_list = ['adult', 'child', 'senior', 'concession']
-    ticket_type = define_ticket_type(ticket_type_list)
+    ticket_type_list = define_ticket_type(init_ticket_type_list)
 
     # Ask for ticket quantity
-    ticket_quantity = define_ticket_quantity(movie_name)
+    ticket_quantity_list, total_ticket = define_ticket_quantity(movie_name)
 
     # Ask for joining reward program
     is_reward_program_list = ['y', 'n']
@@ -192,11 +236,20 @@ def purchase_ticket():
     # Define list of unit price based on ticket type
     unit_price_list = {'adult': 25.0, 'child': 19.5, 'senior': 17.0, 'student': 20.5, 'concession': 20.5}
 
-    # Calculate ticket and booking fees
-    unit_price = unit_price_list[ticket_type]
-    booking_fee = ticket_quantity * 2
-    ticket_cost = ticket_quantity * unit_price
+    # Calculate ticket fees and booking fees
+    ticket_cost = 0
+    ticket_list = []
+    for idx, i in enumerate(ticket_type_list):
+        unit_price = unit_price_list[i]
+        ticket_cost += (ticket_quantity_list[idx] * unit_price)
+        ticket_list.append({
+            CONS_TICKET_TYPE: i,
+            CONS_TICKET_UNIT_PRICE: unit_price,
+            CONS_TICKET_QUANTITY: ticket_quantity_list[idx]
+        })
 
+    booking_fee = total_ticket * 2
+    
     # Calculate discount based on the reward program
     discount = 0
     if is_reward_program:
@@ -207,7 +260,7 @@ def purchase_ticket():
 
     # Recalculate ticket quantity of movie
     movie = retrieve_metadata(init_movie_list, CONS_MOVIE_NAME, movie_name)
-    movie[CONS_MOVIE_SEAT] -= ticket_quantity
+    movie[CONS_MOVIE_SEAT] -= total_ticket
     
     # Add or update current customer to customer list with the reward program information
     customer = retrieve_metadata(init_customer_list, CONS_CUSTOMER_NAME, customer_name)
@@ -229,15 +282,24 @@ def purchase_ticket():
         dash_mgs,
         ('Receipt of {0}').format(customer_name),
         dash_mgs,
-        ('Movie:{0}{1}').format(tab_mgs_four, movie_name),
-        ('Ticket type:{0}{1}').format(tab_mgs_three, ticket_type),
-        ('Ticket unit price:{0}{1}').format(tab_mgs_twice, unit_price),
-        ('Ticket quantity:{0}{1}').format(tab_mgs_twice, ticket_quantity),
+        ('Movie:{0}{1}').format(tab_mgs_four, movie_name)
+    ]
+    
+    # Generate each ticket (with type, unit price and quantity) in the ticket list by iterating
+    for i in ticket_list:
+        receipt_mgs_list.extend([
+            ('Ticket type:{0}{1}').format(tab_mgs_three, i[CONS_TICKET_TYPE]),
+            ('Ticket unit price:{0}{1}').format(tab_mgs_twice, i[CONS_TICKET_UNIT_PRICE]),
+            ('Ticket quantity:{0}{1}').format(tab_mgs_twice, i[CONS_TICKET_QUANTITY])
+        ])
+    
+    # Extend receipt with the discount, booking fee and total cost
+    receipt_mgs_list.extend([
         dash_mgs,
         ('Discount:{0}{1}').format(tab_mgs_three, discount),
         ('Booking fee:{0}{1}').format(tab_mgs_three, booking_fee),
         ('Total cost:{0}{1}').format(tab_mgs_three, total_cost)
-    ]
+    ])
     
     for i in receipt_mgs_list:
         receipt_mgs += break_line_message(i)
@@ -251,13 +313,13 @@ def add_movie():
     movie_list = convert_string_to_list(movie_list_str, CONS_COMMA)
     for i in movie_list:
         i = i.strip() # Strip out all spaces before and after the item
+        # If the entered movie is not the predefined values, add it into the movie list
         if validate_predefined_value(init_movie_list, CONS_MOVIE_NAME, i):
             print(break_line_message('{0} is an existing movie!').format(i))
         else:
             init_movie_list.append({
                 CONS_MOVIE_NAME: i, CONS_MOVIE_SEAT: CONS_TICKET_QUANTITY_MAX
             })
-    #print(init_movie_list)
 
 # Function to display existing customers information (Option 3)
 def display_customer_info():
@@ -269,10 +331,21 @@ def display_movie_info():
     for i in init_movie_list:
         print(('Movie name: {0} | available seat: {1}').format(i[CONS_MOVIE_NAME], i[CONS_MOVIE_SEAT]))
 
+# Function to display the most popular movie (Option 5)
+def display_most_popular_movie():
+    for i in init_movie_list:
+        return 0
+    
+# Function to display all movie record (Option 6)
+def display_movie_record():
+    for i in init_movie_list:
+        return 0
+
 # Function to print out the menu
 def print_menu():
     print(menu_mgs)
     selected_option = input('Choose one option: ')
+    print(CONS_BREAK_LINE)
     match selected_option:
         case '1':
             purchase_ticket()
@@ -282,6 +355,10 @@ def print_menu():
             display_customer_info()
         case '4':
             display_movie_info()
+        case '5':
+            display_most_popular_movie()
+        case '6':
+            display_movie_record()
         case '0':
             quit()
         case _:
